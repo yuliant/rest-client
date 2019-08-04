db1cb05c97c62b8e31493fb0d7799d5c6c9d5cab
Masrizal Eka Yulianto <masrizal04@gmail.com>
2019-08-04 21:03:37 +0700

    make rest client with ci and guzzle 6.0

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -1,63 +1,107 @@
 <?php
 
-/**
- *
- */
+use GuzzleHttp\Client;
+
 class Mahasiswa_model extends CI_model
 {
 
+  private $_client;
+
+  public function __construct()
+  {
+    $this->_client = new Client([
+      'base_uri' => 'http://localhost/project_ci/rest-server/api/',
+      'auth' => ['admin', '1234']
+    ]);
+  }
+
   public function getAllMahasiswa()
   {
-    // $this->db->from($this->table_name);
-    // $this->db->order_by("name", "asc");
-    // $query = $this->db->get(); 
-    // return $query->result();
-    return $this->db->get('mahasiswa')->result_array(); // seperti select * from mahasiswa
+    $response = $this->_client->request('GET', 'mahasiswa', [
+      'query' => [
+        'YULIANT-KEY' => 'masrizal'
+      ]
+    ]);
+
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    return $result['data']; //masuk kedalam object 'data'
   }
+
+
+  public function getMahasiswaById($id)
+  {
+    $response = $this->_client->request('GET', 'mahasiswa', [
+      'query' => [
+        'YULIANT-KEY' => 'masrizal',
+        'id' => $id
+      ]
+    ]);
+
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    return $result['data'][0]; //masuk kedalam object 'data' ambil data ke 1
+  }
+
 
   public function tambahDataMahasiswa()
   {
     $data = [
-        'nama' => $this->input->post('nama', true),
-        'nim' => $this->input->post('nim', true),
-        'email' => $this->input->post('email', true),
-        'jurusan' => $this->input->post('jurusan')
-      ];
-      $this->db->insert('mahasiswa', $data);
-  // Produces: INSERT INTO mytable (title, name, date) VALUES ('My title', 'My name', 'My date')
+      'nama' => $this->input->post('nama', true),
+      'nim' => $this->input->post('nim', true),
+      'email' => $this->input->post('email', true),
+      'jurusan' => $this->input->post('jurusan'),
+      'YULIANT-KEY' => 'masrizal'
+    ];
+    $response = $this->_client->request('POST', 'mahasiswa', [
+      'form_params' => $data
+    ]);
+
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    return $result;
   }
 
   public function hapusDataMahasiswa($id)
   {
-    // $this->db->where('id', $id);
-    // $this->db->delete('mahasiswa');
-    $this->db->delete('mahasiswa', ['id'=>$id]);
-  }
+    $response = $this->_client->request('DELETE', 'mahasiswa', [
+      'form_params' => [
+        'YULIANT-KEY' => 'masrizal',
+        'id' => $id
+      ]
+    ]);
 
-  public function getMahasiswaById($id)
-  {
-    return $this->db->get_where('mahasiswa', ['id'=>$id])->row_array();
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    return $result;
   }
 
   public function ubahDataMahasiswa($id)
   {
     $data = [
-        'nama' => $this->input->post('nama', true),
-        'nim' => $this->input->post('nim', true),
-        'email' => $this->input->post('email', true),
-        'jurusan' => $this->input->post('jurusan')
-      ];
-      $this->db->where('id', $this->input->post('id'));
-      $this->db->update('mahasiswa', $data);
+      'nama' => $this->input->post('nama', true),
+      'nim' => $this->input->post('nim', true),
+      'email' => $this->input->post('email', true),
+      'jurusan' => $this->input->post('jurusan'),
+      'id' => $this->input->post('id'),
+      'YULIANT-KEY' => 'masrizal'
+    ];
+    $response = $this->_client->request('PUT', 'mahasiswa', [
+      'form_params' => $data
+    ]);
+
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    return $result;
   }
 
   public function cariDataMahasiswa()
   {
-      $keyword = $this->input->post('keyword', true);
-      $this->db->like('nama', $keyword);
-      $this->db->or_like('nim', $keyword);
-      $this->db->or_like('email', $keyword);
-      $this->db->or_like('jurusan', $keyword);
-      return $this->db->get('mahasiswa')->result_array();
+    $keyword = $this->input->post('keyword', true);
+    $this->db->like('nama', $keyword);
+    $this->db->or_like('nim', $keyword);
+    $this->db->or_like('email', $keyword);
+    $this->db->or_like('jurusan', $keyword);
+    return $this->db->get('mahasiswa')->result_array();
   }
 }
--- database/phpmvc.sql
+++ database/phpmvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2019 at 02:11 PM
+-- Generation Time: Aug 02, 2019 at 05:20 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -21,6 +21,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `phpmvc`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keys`
+--
+
+CREATE TABLE `keys` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `ip_addresses` text,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `keys`
+--
+
+INSERT INTO `keys` (`id`, `user_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
+(1, 1, 'wpu123', 1, 0, 0, NULL, 1),
+(2, 3, 'masrizal', 1, 0, 0, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `limits`
+--
+
+CREATE TABLE `limits` (
+  `id` int(11) NOT NULL,
+  `uri` varchar(255) NOT NULL,
+  `count` int(10) NOT NULL,
+  `hour_started` int(11) NOT NULL,
+  `api_key` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `limits`
+--
+
+INSERT INTO `limits` (`id`, `uri`, `count`, `hour_started`, `api_key`) VALUES
+(1, 'uri:api/mahasiswa/index:get', 2, 1564712962, 'masrizal'),
+(2, 'uri:api/mahasiswa/index:get', 6, 1564712993, 'wpu123');
 
 -- --------------------------------------------------------
 
@@ -43,12 +90,97 @@ CREATE TABLE `mahasiswa` (
 INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `email`, `jurusan`) VALUES
 (2, 'Risqi Rahmadi Fauzan', '161080200111', 'risqi@umsida.ac.id', 'informatika'),
 (8, 'Masrizal Eka Yulianto', '161080200129', 'masrizal04@gmail.com', 'Informatika'),
-(9, 'Alfiyan Amik', '161080200109', 'alfiyan@gmail.com', 'Teknik Elektro'),
-(10, 'asd', '161080200129', 'masrizal04@gmail.com', 'Teknik Industri');
+(9, 'Alfiyan Amik', '161080200109', 'alfiyan@gmail.com', 'Teknik Elektro');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(3, '2014_10_12_000000_create_users_table', 1),
+(4, '2014_10_12_100000_create_password_resets_table', 1),
+(5, '2019_07_31_050143_create_students_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nim` char(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `nama`, `nim`, `email`, `jurusan`, `created_at`, `updated_at`) VALUES
+(1, 'Masrizal Eka Yulianto', '161080200129', 'masrizal04@gmail.com', 'Informatika', '2019-07-31 05:10:10', NULL),
+(2, 'El Rama', '161080200118', 'janis@gmail.com', 'Informatika', '2019-07-31 05:10:10', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `keys`
+--
+ALTER TABLE `keys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `limits`
+--
+ALTER TABLE `limits`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `mahasiswa`
@@ -57,14 +189,71 @@ ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `students_nim_unique` (`nim`),
+  ADD UNIQUE KEY `students_email_unique` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `keys`
+--
+ALTER TABLE `keys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `limits`
+--
+ALTER TABLE `limits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
